a4eaedd3e87f1bc5e2274a1439ce701d4ed06309
bossiernesto <bossi.ernestog@gmail.com>
2013-10-26 17:04:51 -0300

    agregue formatReporter para la impresion de log del reporter

--- xunit/reporter.rb
+++ xunit/reporter.rb
@@ -1,15 +1,16 @@
 require_relative './resultado'
-require 'colorize'
 
 class Reporter
-  attr_accessor :results
+  attr_accessor :results, :format_reporter, :color
 
-  def initialize
+  def initialize(format_reporter=StringFormatReporter)
     self.reset
+    @format_reporter=format_reporter.new
   end
 
   def reset
     @results = []
+    @color = :green
   end
 
   def success(test)
@@ -21,13 +22,12 @@ class Reporter
   end
 
   def failure(test, message)
-    puts "Failure on test #{test}: #{message}".colorize(:light_yellow)
+    @format_reporter.report_failure(test, message)
     @results << FailedResult.new(test, message)
   end
 
-  def error(test,exception)
-    puts exception.message.red
-    puts exception.backtrace.join("\n").red
+  def error(test, exception)
+    @format_reporter.report_error(test, exception)
     @results << ErrorResult.new(jtest)
   end
 
@@ -39,12 +39,10 @@ class Reporter
       instance.send method
     end
     end_time = Time.now
-    color = :green
     unless self.get_failures.length == 0 and self.get_errors.length == 0
-      color = :red
+      @color = :red
     end
-    puts "#{@results.length} tests,#{self.get_success.length} tests ran ok, #{self.get_failures.length} failures,#{self.get_errors.length} errors.".colorize(color)
-    puts "Finished tests in #{(end_time - beginning_time)*1000} milliseconds".colorize(:cyan)
+    @format_reporter.report_results(beginning_time, end_time, self)
     self.reset
   end
 
@@ -62,6 +60,27 @@ class Reporter
 
   def get_failures
     self.filter_results FailedResult
+  end
+
+end
+
+require 'colorize'
+
+class StringFormatReporter
+
+  def report_error(test, exception)
+    puts exception.message.red
+    puts exception.backtrace.join("\n").red
+  end
+
+  def report_failure(test, message)
+    puts "Failure on test #{test}: #{message}".colorize(:light_yellow)
+  end
+
+  def report_results(beginning_time, end_time, reporter)
+    puts "#{reporter.results.length} tests,#{reporter.get_success.length} tests ran ok, #{reporter.get_failures.length} failures,#{reporter.get_errors.length} errors.".
+             colorize(reporter.color)
+    puts "Finished tests in #{(end_time - beginning_time)*1000} milliseconds".colorize(:cyan)
   end
 
 end
